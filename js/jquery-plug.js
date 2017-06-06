fe6fd31618d4279eecefb1a041cbdeae056ac41a
hao <1027158828@qq.com>
2017-06-06 08:52:00 +0800

    20170606 8:51 修改事件对象 body

--- js/jquery-plug.js
+++ js/jquery-plug.js
@@ -1,10 +1,11 @@
 (function(){
     //左滑动效果
     $.fn.ownSwipeLeft = function(event,classname){
-        $(this).on(event,function(){
-            $(this).addClass(classname);
+        var that = $(this);
+        $('body').on(event,function(){
+            that.addClass(classname);
         });
-        $(this)[0].addEventListener("webkitAnimationEnd", function(){
+        that[0].addEventListener("webkitAnimationEnd", function(){
             this.className="cube";
         }, false);
     };
